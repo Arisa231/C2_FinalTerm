7826e2a5abf20d43922ce0117938d58cbd7994fd
Arisa231 <133213327+Arisa231@users.noreply.github.com>
2023-07-23 22:41:03 +0900

    解答方法の修正。 答え合わせも

--- C2_FinalTerm/C2_FinalTerm.cpp
+++ C2_FinalTerm/C2_FinalTerm.cpp
@@ -17,23 +17,45 @@ int main()
 	printQuestion(char1, char2, size, answer);
 
 	fprintf_s(stdout, "\n\n");
-	fprintf_s(stdout, u8"一つだけ異なる文字は、何列目ですか？(解答を入力したらEnterを押してください。\n");
-	double d;
+	double d1;
+	double d2;
+	int answerPlayer = 0;
 	
+	// 行の入力
 	while (1) {
+		fprintf_s(stdout, u8"一つだけ異なる文字は、何行目ですか？(解答を入力したらEnterを押してください。\n");
 		fprintf_s(stdout, u8"解答：");
-		scanf_s("%lf", &d);
-
-		if (d > size or d < 1) {
-			fprintf_s(stdout, u8"1から%d列までしかありません。もう一度入力してください。\n", size);
-		}
-		else if (isdigit(d) == false) {
-			fprintf_s(stdout, u8"数字を入力してください。\n");
-		}
-		else {
+		if (scanf_s("%lf", &d1) == 1 and d1 <= size and d1 > 0)
 			break;
-		}
+		fprintf_s(stdout, u8"1から%d行目までしかありません。もう一度入力してください。\n", size);
+		scanf_s("%*[^\n]");
 	}
+	
+	answerPlayer = d1;
+
+	fprintf_s(stdout, "\n");
+
+	// 列の入力
+	while (1) {
+		fprintf_s(stdout, u8"何列目ですか？(解答を入力したらEnterを押してください。\n");
+		fprintf_s(stdout, u8"解答：");
+		if (scanf_s("%lf", &d2) == 1 and d2 <= size and d2 > 0)
+			break;
+		fprintf_s(stdout, u8"1から%d列目までしかありません。もう一度入力してください。\n", size);
+		scanf_s("%*[^\n]");
+	}
+
+	answerPlayer = answerPlayer * 10 + d2;
+
+	fprintf_s(stdout, "%d", answerPlayer);
+
+	if (answer == answerPlayer) {
+		fprintf_s(stdout, u8"正解です！\n");
+	}
+	else {
+		fprintf_s(stdout, u8"残念、不正解です.\n");
+	}
+	
 
 
 
